8711b02010db5c3c84011bcc5b43c84ff7dc900d
Francesco Nwokeka <francesco.nwokeka@gmail.com>
2011-11-21 21:39:30 +0100

    Fix namespace typo
    and sorted out some of Simone's bad english

--- include/flags.h
+++ include/flags.h
@@ -2,30 +2,30 @@
  * libAgatha library
  *
  * This file is part of the libAgatha library
- * Copyright (C) 2011 Francesco Nwokeka <francesco.nwokeka@gmail.com>
+ * Copyright (C) 2011 Simone Daminato <skyled@alice.it>
  *
  */
 
 #ifndef FLAGS_H
 #define FLAGS_H
 
-namespace agatha {
+namespace Agatha {
 
-    /** 
+    /**
      * @file flags.h
-     * @brief This file defines the common stuffs of the library, like enums.
+     * @brief This file defines the common stuff for the Agatha library, like enums.
     */
-    
+
     /**
     * @enum CallError
-    * @brief specifies the error type
+    * @brief specifies the error type when talking to Agatha
     * @author Daminato Simone
     *
-    * Specifies the nature of the error occurred.
+    * Specifies the nature of the error that occurred whilst communicating with Agatha
     */
     enum CallError {
-        SERVER_TIMEOUT,    /** a timeout occurred waiting for the Agatha server. */
-        INVALID_TOKEN,     /** the given token is not valid. */
+        SERVER_TIMEOUT,    /** a timeout occurred waiting for the Agatha server to respond */
+        INVALID_TOKEN,     /** the given token is not valid */
     };
 
 };
