65600bbf831ebdfdb82815437d2f9d64cc87fe53
zamy <skyled@alice.it>
2011-11-22 15:44:27 +0100

    Finita creazione di AgathaCallback.
    Corretti errori e fatta l'implementazione, ora è completamente funzionante.

---
+++ src/agathaCallback.cpp
@@ -0,0 +1,14 @@
+/*
+ * libAgatha library
+ *
+ * This file is part of the libAgatha library
+ * Copyright (C) 2011 Simone Daminato <skyled@alice.it>
+ *
+ */
+#include "../include/agathaCallback.h"
+
+namespace Agatha {
+    
+    AgathaCallback::AgathaCallback(){};
+    AgathaCallback::~AgathaCallback(){};
+};
