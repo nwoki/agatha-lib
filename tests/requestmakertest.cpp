cff70c12d8db446e2d3cd5b1d3f72a8495140f58
zamy <skyled@alice.it>
2012-02-08 18:01:12 +0100

    Correzione al test di requestmaker.

--- tests/requestmakertest.cpp
+++ tests/requestmakertest.cpp
@@ -28,17 +28,18 @@ void RequestMakerTest::testCreateRequest()
 {
     /// TODO need to test for ALL different request types!
     Player *player = new Player("[LPG]KiicK-aSS", "GLAORAA", "90.54.12.78", "00000111220000020002", "kajnwdoooAAISON12314d");
+    std::string token ("test");
 
     // Results to check with
-    QByteArray addPlayerResult("{ \"command\" : \"add\", \"game\" : \"URT_411\", \"playerInfo\" : { \"gear\" : \"GLAORAA\", \"guid\" : \"kajnwdoooAAISON12314d\", \"ip\" : \"90.54.12.78\", \"name\" : \"[LPG]KiicK-aSS\", \"weaponMode\" : \"00000111220000020002\" } }");
-    QByteArray banResult("{ \"command\" : \"ban\", \"game\" : \"URT_411\", \"playerInfo\" : { \"gear\" : \"GLAORAA\", \"guid\" : \"kajnwdoooAAISON12314d\", \"ip\" : \"90.54.12.78\", \"name\" : \"[LPG]KiicK-aSS\", \"weaponMode\" : \"00000111220000020002\" } }");
-    QByteArray isBannedResult("{ \"command\" : \"isBanned\", \"game\" : \"URT_411\", \"playerInfo\" : { \"gear\" : \"GLAORAA\", \"guid\" : \"kajnwdoooAAISON12314d\", \"ip\" : \"90.54.12.78\", \"name\" : \"[LPG]KiicK-aSS\", \"weaponMode\" : \"00000111220000020002\" } }");
+    QByteArray addPlayerResult("{ \"command\" : \"add\", \"game\" : \"URT_411\", \"playerInfo\" : { \"gear\" : \"GLAORAA\", \"guid\" : \"kajnwdoooAAISON12314d\", \"ip\" : \"90.54.12.78\", \"name\" : \"[LPG]KiicK-aSS\", \"weaponMode\" : \"00000111220000020002\" }, \"token\" : \"test\" }");
+    QByteArray banResult("{ \"command\" : \"ban\", \"game\" : \"URT_411\", \"playerInfo\" : { \"gear\" : \"GLAORAA\", \"guid\" : \"kajnwdoooAAISON12314d\", \"ip\" : \"90.54.12.78\", \"name\" : \"[LPG]KiicK-aSS\", \"weaponMode\" : \"00000111220000020002\" }, \"token\" : \"test\" }");
+    QByteArray isBannedResult("{ \"command\" : \"isBanned\", \"game\" : \"URT_411\", \"playerInfo\" : { \"gear\" : \"GLAORAA\", \"guid\" : \"kajnwdoooAAISON12314d\", \"ip\" : \"90.54.12.78\", \"name\" : \"[LPG]KiicK-aSS\", \"weaponMode\" : \"00000111220000020002\" }, \"token\" : \"test\" }");
 
-    qDebug() << "REQUEST_MAKER MESSAGE: " << m_requestMaker->createRequest(RequestMaker::URT_411, RequestMaker::ADD_PLAYER, player);
+    qDebug() << "REQUEST_MAKER MESSAGE: " << m_requestMaker->createRequest(token, RequestMaker::URT_411, RequestMaker::ADD_PLAYER, player);
 
-    QCOMPARE(m_requestMaker->createRequest(RequestMaker::URT_411, RequestMaker::ADD_PLAYER, player), addPlayerResult);  // addPlayer
-    QCOMPARE(m_requestMaker->createRequest(RequestMaker::URT_411, RequestMaker::BAN, player), banResult);               // ban
-    QCOMPARE(m_requestMaker->createRequest(RequestMaker::URT_411, RequestMaker::IS_BANNED, player), isBannedResult);    // isBanned
+    QCOMPARE(m_requestMaker->createRequest(token, RequestMaker::URT_411, RequestMaker::ADD_PLAYER, player), addPlayerResult);  // addPlayer
+    QCOMPARE(m_requestMaker->createRequest(token, RequestMaker::URT_411, RequestMaker::BAN, player), banResult);               // ban
+    QCOMPARE(m_requestMaker->createRequest(token, RequestMaker::URT_411, RequestMaker::IS_BANNED, player), isBannedResult);    // isBanned
 }
 
 QTEST_MAIN(RequestMakerTest)
